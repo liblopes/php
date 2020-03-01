45261af9020f423ee11d1bd9ce17008457969539
liblopes <55331382+liblopes@users.noreply.github.com>
2020-03-01 20:40:41 -0300

    Create exercicio01.php

---
+++ exercicio01.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '<h1>Exercicio 1</h1>
+		<form action="./exercicio01.php" method="post">
+			<input type="number" name="numero1" placeholder="primeiro numero" required="">
+			<input type="number" name="numero2" placeholder="segundo numero" required="">
+			<br/>
+			<input type="submit" value="OK">
+		</form>';
+
+$a = $_POST['numero1'];
+$b = $_POST['numero2'];
+$x = $a+$b;
+
+echo ("A soma de {$a} e {$b} é: {$x}");
+
+?>
