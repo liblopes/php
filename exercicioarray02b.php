0ce5ffa0d79c24bffd443b724f4b3d1e5ca3d864
liblopes <55331382+liblopes@users.noreply.github.com>
2020-03-01 22:14:41 -0300

    Create exercicioarray02b.php

---
+++ exercicioarray02b.php
@@ -0,0 +1,21 @@
+<?php
+// prompt abre a caixinha de texto sobre a tela, diferentemente do input que cria uma caixa na pagina
+//document.write cria um objeto dentro do html com o valor que foi digitado no prompt 
+
+$nome= "<script> var nome= prompt ('Insira o nome');document.write(nome)  </script>";
+
+$telefone = "<script> var telefone = prompt('Insira o telefone'); document.write(telefone)</script>";
+
+$endereco = "<script> var rua = prompt('Insira o rua'); document.write(rua)</script>";
+
+
+$dados= array();
+$dados[0]=$nome;
+$dados[1]=$endereco;
+$dados[2]=$telefone;
+
+
+
+var_dump($dados);
+print_r($dados);
+?>
