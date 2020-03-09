f305c961d95403971713c4efedc9c40b1d74a629
liblopes <55331382+liblopes@users.noreply.github.com>
2020-03-09 18:34:58 -0300

    Update and rename exercicio.operacoesswitch.php to exercicio.menu.switch.php

---
+++ exercicio.menu.switch.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+<form action="./exercicio.menu.php" method="post">
+
+    <h1> Menu operações </h1>
+
+    <input type="number" name="numero1" placeholder="numero 1" required="">
+			<input type="number" name="numero2" placeholder="numero 2" required="">
+			<br/>
+            <br/>
+
+    <select name="operacao">
+        <option> Somar </option>
+        <option> Subtrair </option>
+        <option> Multiplicar </option>
+        <option> Dividir </option>
+    </select>
+    <br><br>
+
+    <input type="submit" value="OK">
+    <br><br>
+			
+		</form>
+
+</html>
+
+<?php
+
+$numero1 = $_REQUEST['numero1'];
+$numero2 = $_REQUEST['numero2'];
+$operacao = $_REQUEST['operacao'];
+
+
+
+        switch($operacao){
+
+            case 'Somar':
+            $soma = $numero1 + $numero2;
+
+            echo "Soma = $soma";
+        break;
+
+            case 'Subtrair';
+            $subtrair = $numero1 - $numero2;
+
+            echo "Subtração = $subtrair";
+        break;
+
+            case 'Multiplicar';
+            $multiplicar = $numero2 * $numero1;
+
+            echo "Multiplicação = $multiplicar";
+        break;
+
+            case 'Dividir';
+            $dividir = $numero1 / $numero2;
+
+            echo "Divisão = $dividir";
+        break;
+
+        }
+
+
+?>
