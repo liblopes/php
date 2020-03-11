98196125716b968bfb612bf148f5ae4b036bb365
liblopes <55331382+liblopes@users.noreply.github.com>
2020-03-11 12:36:50 -0300

    Create exercicio.tabuada.php

---
+++ exercicio.tabuada.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<form action="tabuada.php" method="post">
+
+    <h1> Tabuada </h1>
+
+    <input type="number" name="numero" placeholder="Digite aqui" required="">
+
+    <input type="submit" value= "OK">
+
+    <br><br>
+
+    </form>			
+
+</html>
+
+<?php
+
+if(isset($_POST['numero'])){
+    $numero = $_POST['numero'];
+}
+if(isset($_POST['i'])){
+     $i = $_POST['i'];
+}
+for($i=0; $i<=10; $i++){
+
+    $tabuada = $numero*$i;
+   
+
+    echo "{$numero} x {$i} = {$tabuada}\n\n";
+    
+
+}
+
+?>
